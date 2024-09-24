af795e8bba820b97df78db990ef107d62cc85d1f
goldenhippo58 <zkleckner@gmail.com>
2024-09-23 22:25:06 -0400

    Update Kyber-Rust to version 0.2.0

--- src/bindings.rs
+++ src/bindings.rs
@@ -3,4 +3,8 @@ extern "C" {
     pub fn pqcrystals_kyber768_ref_keypair(pk: *mut u8, sk: *mut u8) -> i32;
     pub fn pqcrystals_kyber768_ref_enc(ct: *mut u8, ss: *mut u8, pk: *const u8) -> i32;
     pub fn pqcrystals_kyber768_ref_dec(ss: *mut u8, ct: *const u8, sk: *const u8) -> i32;
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> be76552 (Initial commit of Kyber-Rust library)
